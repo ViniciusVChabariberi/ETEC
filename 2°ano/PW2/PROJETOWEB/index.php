7a59fe9e52b43a5a06e0cf58ac3dd4fa75fd94ac
vini <vini.valero@gmail.com>
2023-02-06 16:34:46 -0300

    aula de php

---
+++ 2°ano/PW2/PROJETOWEB/index.php
@@ -0,0 +1,9 @@
+<?php
+    $a = 1;
+    $b = 2;
+
+    echo "Adição: " . $a + $b . "<br>";
+    echo "Subtração: " . $a - $b . "<br>";
+    echo "Multiplicação: " . $a * $b . "<br>";
+    echo "Divisão: " . $a / $b . "<br>";
+?>
