1672e2320e1cd02b5adc7af3cb96465c9514401f
vinicius <vini.valero@gmail.com>
2023-02-27 15:28:56 -0300

    aula de php

---
+++ 2°ano/PW2/Aula27_02/index.php
@@ -0,0 +1,31 @@
+<?php
+//criação de variável
+$a = 0;
+
+//Sistema de decisão
+$b = 10;
+
+if ($b <= 10) {
+    echo "O valor da variável é maior igual a 10";
+} else {
+    echo "O valor da variável é menor que 10";
+}
+
+
+//criação da repetição
+for ($i=0; $i <= 10; $i++) {
+    echo $i;
+}
+
+$x = 0;
+while ($x <= 10) {
+    echo $x;
+}
+
+$y = 0;
+do {
+    echo $y;
+    $y++;
+} while ($y <= 10);
+
+?>
