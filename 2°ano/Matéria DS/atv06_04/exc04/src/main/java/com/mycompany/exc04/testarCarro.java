2ef5900f4508f72865b85390142d922a22e80abd
vinicius <vini.valero@gmail.com>
2023-04-06 15:26:18 -0300

    adicionado

---
+++ 2°ano/Matéria DS/atv06_04/exc04/src/main/java/com/mycompany/exc04/testarCarro.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.exc04;
+
+/**
+ *
+ * @author dti
+ */
+public class testarCarro {
+    public static void main(String[] args){
+        teste caro = new teste("Volkswagen" , "Golf" , "Azul" , "AGS-0404");
+    }
+}
