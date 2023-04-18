490fabadf3e44bfdb5b8a4f889625c3ef057941d
Vini_Beri <99839128+ViniciusVChabariberi@users.noreply.github.com>
2023-04-17 22:15:16 -0300

    Add files via upload

--- 2°ano/PW2/aula10_04/index.php
+++ 2°ano/PW2/aula10_04/index.php
@@ -1,64 +1,48 @@
 <?php
 
 
-    if(isset($_POST['votob'])){
-        $voto = fopen("voto.txt" , "a");
-        fwrite($voto , "Voto: branco" . "\n");
-        fclose($voto);
-    } else if(isset($_POST['votov'])){
-        $voto = fopen("voto.txt" , "a");
-        fwrite($voto , "Voto: vermelho " . "\n");
-        fclose($voto);
-    }
+if(isset($_POST['branco'])){
+    $voto = fopen("dados.txt", "a");
+    fwrite($voto, $_POST['branco'] . "\n");
+    fclose($voto); 
+}else if(isset($_POST['vermelho'])){
+    $voto = fopen("dados.txt", "a");
+    fwrite($voto, $_POST['vermelho'] . "\n");
+    fclose($voto); 
+}
 
     function Registrar(){
         $voto = "dados.txt";
+        $vt = fopen($voto , "r");
+        $total = fread($vt , filesize($voto));
 
-        $contarVotoBranco = fopen($voto, "r");
-        $votoBranco = fread($contarVotoBranco, filesize($voto));
-        $branco = substr_count(' '.$votoBranco.' ', 'Voto Registrado: Branco');
+		$votoVermelho = 0;
+        $votoBranco = 0;
 
-        $contarVotoVerde = fopen($voto, "r");
-        $votoVerde = fread($contarVotoVerde, filesize($voto));
-        $verde = substr_count(' '.$votoVerde.' ', 'Voto Registrado: Verde');
+        $votoVermelho = substr_count($total , "Vermelho");
+        $votoBranco = substr_count($total , "Branco");
 
-        if($verde<$branco){
-            $voto = fopen("dados.txt" , "a");
-            fwrite($voto, "Voto verde venceu " . $verde . " votos \n");
-			fclose($voto);
-			echo "<p>Voto verde: " . $verde . " votos.<p>";
-			echo "<p>Voto branco: " . $branco . " votos.</p>";
-			echo "<p>Voto verde venceu.</p>";
-        } else if($branco<$verde){
-            $voto = fopen("dados.txt" , "a");
-            fwrite($voto, "Voto branco venceu " . $branco . " votos \n");
-			fclose($voto);
-			echo "<p>Voto branco: " . $branco . " votos.<p>";
-			echo "<p>Voto verde: " . $verde . " votos.</p>";
-			echo "<p>Voto branco venceu.</p>";
-        }else if($branco == $verde){
-            if($branco == 1){
-                $voto = fopen("dados.txt" , "a");
-                fwrite($voto, "O voto verde empatou com o voto branco. Quantidade de votos " . $verde . "voto cada. \n");
-                fclose($voto);
-                echo "<p>Voto verde: " . $verde . " voto.<p>";
-                echo "<p>Voto branco: " . $branco . " voto.</p>";
-                echo "<p>Empate.</p>";
-            } else if {
-                $voto = fopen("dados.txt" , "a");
-                fwrite($voto, "O voto verde empatou com o voto branco. Quantidade de votos " . $verde . "voto cada. \n");
-                fclose($voto);
-                echo "<p>Voto verde: " . $verde . " voto.<p>";
-                echo "<p>Voto branco: " . $branco . " voto.</p>";
-                echo "<p>Empate.</p>";
-            }
-        }
-    }
-    
-    if($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['Registrar'])){
-        Registrar();
-    }
 
+		if($branco > $vermelho){
+			echo "<p>branco: " . $branco . " votos.</p>";
+			echo "<p>vermelho: " . $vermelho . " votos.</p>";
+			echo "<p>branco venceu.</p>";
+		}else if($vermelho > $branco){
+			echo "<p>vermelho: " . $vermelho . " votos.</p>";
+			echo "<p>branco: " . $branco . " votos.</p>";
+			echo "<p>vermelho venceu.</p>";
+		}else if($vermelho == $branco){
+			if($vermelho == 1){
+				echo "<p>branco: " . $branco . " voto.</p>";
+				echo "<p>vermelho: " . $vermelho . " voto.</p>";
+				echo "<p>Empate.</p>";
+ 			}else if($vermelho > 1){
+				echo "<p>branco: " . $branco . " votos.</p>";
+				echo "<p>vermelho: " . $vermelho . " votos.</p>";
+				echo "<p>Empate.</p>";
+			}
+		}
+	}
 ?>
 
 <!DOCTYPE html>
@@ -67,16 +51,33 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
-<body style="background-color: <?php if(isset($_POST['nome'])) { echo '#CCCCCC'; } else { echo 'f3f3f3'; }?>">
-    <form name="dados_pessoais" method="POST" action="">
-       <br> Votação <br>
-
-        Branco: <input type="radio" name="votob" id="votob"><br>
-        Vermelho: <input type="radio" name="votov" id="votov"><br>
-        <input type="submit" name="cadastrar" id="cadastrar" value="Cadastrar">
-    </form>
+<body>
+    <div class="container text-center">
+        <form method="POST" action="">
+            <div class="row">
+                <div class="col">
+                    <h2> Votação <h2>
+                </div>
+                <div class="col">
+                    Branco: <input type="submit" name="branco" id="branco"><br>
+                </div> 
+                <div class="col">
+                    Vermelho: <input type="submit" name="vermelho" id="vermelho"><br>
+                </div>    
+            </div>    
+        </form>    
+        <form action="resultado.php">
+            <div class="row">
+                <div class="col">
+                    <input type="submit" name="Registrar" id="Registrar" value="registrar">
+                </div>
+            </div>    
+        </form>
+    </div>        
 </body>
 </html>
 
