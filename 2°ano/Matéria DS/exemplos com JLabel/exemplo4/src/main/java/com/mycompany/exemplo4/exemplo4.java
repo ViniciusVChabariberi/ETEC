59d5aab6d7291b1044bb93a5169d673ece5a4e83
vinicius <vini.valero@gmail.com>
2023-04-20 10:36:06 -0300

    adicionado o exemplo5 e modificando o exemplo4

--- 2°ano/Matéria DS/exemplos com JLabel/exemplo4/src/main/java/com/mycompany/exemplo4/exemplo4.java
+++ 2°ano/Matéria DS/exemplos com JLabel/exemplo4/src/main/java/com/mycompany/exemplo4/exemplo4.java
@@ -13,47 +13,50 @@ import java.awt.*;
 //criação de classe pública que herda o JFrame
 public class exemplo4 extends JFrame  {
     //declaração dos objetos
-    JLabel titulo,rotulo1,rotulo2,rotulo3;
+    JLabel rotulo1,rotulo2,rotulo3;
     JTextField texto1,texto2,texto3;
+    JButton botao1, botao2;
     public exemplo4(){
         super("Exemplo com JTextField");
         Container tela = getContentPane();
         setLayout(null);
             //define o nome dos elementos
-            titulo = new JLabel("Cadastro de Clientes");
             rotulo1 = new JLabel("Nome:");
             rotulo2 = new JLabel("Senha:");
             rotulo3 = new JLabel("Comentário:");
+            
+            botao1 = new JButton ("OK");
+            botao2 = new JButton ("Cancelar");
             //campo de texto
             texto1 = new JTextField(100);
-            texto2 = new JTextField(30);
-            texto3 = new JTextField(10);
+            texto2 = new JTextField(100);
+            texto3 = new JTextField(100);
             //define o tamanho e a posição dos elementos
-            titulo.setBounds(180, 15, 160, 20);
-            rotulo1.setBounds(50,50,80,20);
-            rotulo2.setBounds(50,100,80,20);
-            rotulo3.setBounds(50,150,90,20);
+            rotulo1.setBounds(50,20,80,20);
+            rotulo2.setBounds(50,70,80,20);
+            rotulo3.setBounds(50,120,90,20);
 
-            texto1.setBounds(50,70,200,20);
-            texto2.setBounds(50,120,150,20);
-            texto3.setBounds(50,170,130,20);
+            texto1.setBounds(50,40,400,20);
+            texto2.setBounds(50,90,400,20);
+            texto3.setBounds(50,140,400,200);
+            
+            botao1.setBounds(50,180,80,40);
+            botao2.setBounds(100,180,150,40);
             //foco para a caixa de texto
             texto1.requestFocus();
             texto2.requestFocus();
             texto3.requestFocus();
             //define a cor dos elementos
-            titulo.setForeground(Color.red);
             rotulo1.setForeground(Color.black);
             rotulo2.setForeground(Color.black);
             rotulo3.setForeground(Color.black);
             //define a cor do texto da caixa de texto
             texto1.setForeground(Color.black);
             //define a cor de fundo do container
-            tela.setBackground(new Color(216,212,196));
+            tela.setBackground(Color.white);
             //define a cor de fundo da caixa de texto
             texto1.setBackground(Color.white);
             //define a fonte do texto
-            titulo.setFont(new Font("Arial",Font.BOLD,14));
             rotulo1.setFont(new Font("Arial",Font.BOLD,14));
             rotulo2.setFont(new Font("Arial",Font.BOLD,14));
             rotulo3.setFont(new Font("Arial",Font.BOLD,14));
@@ -65,13 +68,14 @@ public class exemplo4 extends JFrame  {
                 //texto1.setHorizontalAlignment(JTextField.CENTER);
                 
             //adiciona os elemento para o container
-            tela.add(titulo);
             tela.add(rotulo1);
             tela.add(rotulo2);
             tela.add(rotulo3);
             tela.add(texto1);
             tela.add(texto2);
             tela.add(texto3);
+            tela.add(botao1);
+            tela.add(botao2);
             //tamanho da janela gráfica
             setSize(500,450);
             setVisible(true);
---
+++ 2°ano/Matéria DS/exemplos com JLabel/exemplo5/src/main/java/com/mycompany/exemplo5/Exemplo5.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exemplo5;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+/**
+ *
+ * @author viniv
+ */
+public class Exemplo5 extends JFrame{
+    JButton botao1, botao2, botao3, botao4;
+    ImageIcon icone;
+    public Exemplo5(){
+        super("Exemplo com JButton");
+        Container tela = getContentPane();
+        setLayout(null);
+        icone = new ImageIcon("abrir.gif");
+        botao1 = new JButton ("Procurar",icone);
+        botao2 = new JButton ("Voltar <<");
+        botao3 = new JButton ("Próximo >>");
+        botao4 = new JButton ("Abrir");
+        
+        botao1.setBounds(50, 20, 100, 20);
+        botao2.setBounds(50, 60, 100, 20);
+        botao3.setBounds(50, 100, 100, 20);
+        botao4.setBounds(50, 140, 100, 20);
+        
+        botao1.setBackground(Color.black);
+        botao2.setBackground(Color.black);
+        botao3.setBackground(Color.black);
+        botao4.setBackground(Color.black);
+        
+        botao1.setForeground(Color.white);
+        botao2.setForeground(Color.white);
+        botao3.setForeground(Color.white);
+        botao4.setForeground(Color.white);
+        //foco em uma letra
+        botao1.setMnemonic(KeyEvent.VK_P);
+        botao1.setMnemonic(KeyEvent.VK_V);
+        botao1.setMnemonic(KeyEvent.VK_X);
+        botao1.setMnemonic(KeyEvent.VK_S);
+        //tecla enter pressionada com foco
+        getRootPane().setDefaultButton(botao1);
+        
+        tela.add(botao1);
+        tela.add(botao2);
+        tela.add(botao3);
+        tela.add(botao4);
+        setSize(400, 250);
+        setVisible(true);
+        setLocationRelativeTo(null);
+    }
+    public static void main(String[] args) {
+        Exemplo5 app = new Exemplo5();
+        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
