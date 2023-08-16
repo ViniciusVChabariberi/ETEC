1d4a8dc969f8e5d555530339898b8e66a4f98ce0
Vini_Beri <99839128+ViniciusVChabariberi@users.noreply.github.com>
2023-08-16 14:20:44 -0300

    Projeto Imc

---
+++ 2°ano/Matéria DS/DiagramaCodigo/ProjetoImc/src/main/java/com/mycompany/projetoimc/Principal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projetoimc;
+
+/**
+ *
+ * @author dti
+ */
+public class Principal {
+        public static void main(String[] args){
+            MenuImc menuPrincipal = new MenuImc();
+            menuPrincipal.executarIMC();
+	}
+}
