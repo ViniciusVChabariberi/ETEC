3f70fe1f987cc28be6a5c1a1917518e1d15c1382
Vinicius <vini.valero@gmail.com>
2024-09-16 11:48:52 -0300

    atualizando arquivos do 3ºano

--- 3ºano/PAM 2/Aula1/atv16/src/Main.kt
+++
@@ -1,9 +0,0 @@
-package fundamentos.controles
-fun main(args: Array<String>) {
-    for (i in 0 .. 100 step 5) {
-        println(i)
-    }
-    for (i in 100 downTo 0 step 5) {
-        println(i)
-    }
-}
--- 3ºano/PAM 2/Aula1/atv6/src/Main.kt
+++
@@ -1,5 +0,0 @@
-package fundamentos
-fun main(args: Array<String>) {
-    val bomHumor = true
-    print("Hoje estou ${if (bomHumor) "Feliz" else "chateado"}")
-}
